5c6c83b79b307a1a6f366c25965b4cc2276c41a7
martinweiler <mweiler@redhat.com>
2017-07-12 10:51:05 +0200

    KSessionRefreshServlet provides functionality to re-initialize the singleton ksession in BRMS 5.3.1

---
+++ README.txt
@@ -0,0 +1,48 @@
+# jBPM 5 ksession refresh servlet for BRMS 5.3.1
+
+This is a WAR file which allows to re-initialize the ksession instance in StatefulKnowledgeSessionUtil.SessionHolder. 
+The reasons for creating this servlet is outlined in Red Hat KBase https://access.redhat.com/solutions/3111691.
+
+- mvn clean package
+- Copy target/jbpm5-refresh-ksession-servlet-<VERSION>.jar to $BRMS_HOME/server/$PROFILE/deploy/deploy/business-central-server.war/WEB-INF/lib/
+- Edit $BRMS_HOME/server/$PROFILE/deploy/business-central-server.war/WEB-INF/web.xml, add:
+
+~~~
+    <!-- ADD SERVLET DEFINITION AND MAPPING -->
+    <servlet>
+        <servlet-name>KSessionRefreshServlet</servlet-name>
+        <servlet-class>org.jbpm.integration.console.KSessionRefreshServlet</servlet-class>
+        <load-on-startup>1</load-on-startup>
+    </servlet>
+
+    <servlet-mapping>
+        <servlet-name>KSessionRefreshServlet</servlet-name>
+        <url-pattern>/ksession-refresh</url-pattern>
+    </servlet-mapping>
+    
+    <!-- ADD URL TO RESOURCE CONSTRAINT TO PREVENT UNAUTHORIZED ACCESS -->
+  <security-constraint>
+    <web-resource-collection>
+      <web-resource-name>Resteasy</web-resource-name>
+      <!-- ADD AFTER EXISTING URL PATTERNS: -->
+      <url-pattern>/ksession-refresh/*</url-pattern>
+    </web-resource-collection>
+~~~
+
+- During the start of the server, the servlet emits a log to inform about it's initialization, but does not perform further actions:
+2017-07-06 10:42:08,429 INFO  [org.jbpm.integration.console.KSessionRefreshServlet] (main) KSession refresh servlet initialized
+
+To invoke the servlet, issue a GET request to the following URL:
+http://$HOST:$PORT/business-central-server/ksession-refresh?action=refresh
+
+This will result in the following logging:
+
+10:11:50,977 INFO  [KSessionRefreshServlet] Servlet called with action: refresh
+...(logging output from ksession re-initialization)...
+10:11:51,853 INFO  [KSessionRefreshServlet] StatefulKnowledgeSession in SessionHolder refreshed
+10:11:51,904 INFO  [KSessionRefreshServlet] Number of processes: 3
+10:11:51,904 INFO  [KSessionRefreshServlet]          second-process
+10:11:51,904 INFO  [KSessionRefreshServlet]          test-process
+10:11:51,904 INFO  [KSessionRefreshServlet]          third-process
+10:11:51,905 INFO  [KSessionRefreshServlet] Session 1 successfully refreshed
+
---
+++ src/main/java/org/jbpm/integration/console/KSessionRefreshServlet.java
@@ -0,0 +1,76 @@
+package org.jbpm.integration.console;
+
+import java.lang.reflect.Field;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.drools.definition.process.Process;
+import org.drools.io.ResourceFactory;
+
+public class KSessionRefreshServlet extends HttpServlet {
+
+    private static Logger logger = Logger.getLogger(KSessionRefreshServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+      logger.info("KSession refresh servlet initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String action = req.getParameter("action");
+        log("Servlet called with action: " + action, writer, "h3");
+        try {
+           if("refresh".equals(action)) {
+
+              // dispose session and agent 
+              StatefulKnowledgeSessionUtil.getStatefulKnowledgeSession().dispose();
+              StatefulKnowledgeSessionUtil.getKagent().dispose();
+              
+              // stop existing scanner/notifier to avoid threak leak
+              ResourceFactory.getResourceChangeScannerService().stop();
+              ResourceFactory.getResourceChangeNotifierService().stop();
+
+              // make private SessionHolder accessible by reflection
+              Class<?> sessionHolderClass = StatefulKnowledgeSessionUtil.class.getDeclaredClasses()[0];
+              Field statefulKnowledgeSessionField = sessionHolderClass.getField("statefulKnowledgeSession");
+              statefulKnowledgeSessionField.setAccessible(true);
+              
+              // re-initialize the SessionHolder.statefulKnowledgeSession property
+              statefulKnowledgeSessionField.set(StatefulKnowledgeSessionUtil.class, StatefulKnowledgeSessionUtil.initializeStatefulKnowledgeSession());
+              log("StatefulKnowledgeSession in SessionHolder refreshed", writer, "p");
+              
+              // display the number of processes
+              List<Process> processes = CommandDelegate.getProcesses();
+              log("Number of processes: " + (processes != null ? processes.size() : "null"), writer, "p");
+              for(Process p : processes) {
+               log("         " + p.getName(), writer, "li");
+              }
+              log("Session " + StatefulKnowledgeSessionUtil.getStatefulKnowledgeSession().getId() + " successfully refreshed", writer, "p");
+           } 
+           else {
+              log("Unknown action " + action + "! To force a session re-initialization, call the servlet with ?action=refresh", writer, "p");
+           }
+        }catch(Exception ex) {
+            logger.error(ex);
+            log("ERROR: " + ex.getMessage(), writer, "b");
+        }finally {
+            writer.close();
+        }
+    }
+
+    // helper method to print to log and to http response
+    private void log(String msg, PrintWriter writer, String htmlDecoration) {
+      logger.info(msg);
+      writer.write("<" + htmlDecoration + ">" + msg + "</" + htmlDecoration + ">");
+    }
+}
